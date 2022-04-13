e4890c6ce54c114b2a4a9bd9b4010796e36bea13
ibilalchaudhary <bilalchaudhary478@gmail.com>
2022-04-13 18:33:52 +0500

    login redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
-import React from "react"
-import { Route, Routes } from "react-router"
-import HomeScreen from "./screens/HomeScreen"
-import "./App.scss"
-import Register from "./screens/Register"
-import RegisterIndividual from "./screens/RegisterIndividual"
-import Login from "./screens/Login"
-import AboutUs from "./screens/AboutUs"
-import SellBitCoin from "./screens/SellBitCoin"
-import Transaction from "./screens/Transaction"
-import BuyBitCoin from "./screens/BuyBitCoin"
-import TradeForMe from "./screens/TradeForMe"
-import SideLogin from "./screens/SideLogin"
-import SideRegister from "./screens/SideRigester"
-import ForexTraning from "./screens/ForexTraning"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import React, { useState } from "react";
+import { Route, Routes } from "react-router";
+import HomeScreen from "./screens/HomeScreen";
+import "./App.scss";
+import Register from "./screens/Register";
+import RegisterIndividual from "./screens/RegisterIndividual";
+import Login from "./screens/Login";
+import AboutUs from "./screens/AboutUs";
+import SellBitCoin from "./screens/SellBitCoin";
+import Transaction from "./screens/Transaction";
+import BuyBitCoin from "./screens/BuyBitCoin";
+import TradeForMe from "./screens/TradeForMe";
+import SideLogin from "./screens/SideLogin";
+import SideRegister from "./screens/SideRigester";
+import ForexTraning from "./screens/ForexTraning";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
+  const [noHeaderFooter, setNoHeaderFooter] = useState(false);
   return (
     <>
-      <Header />
+      {noHeaderFooter ? null : <Header />}
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/register" element={<Register />} />
         <Route path="/individual-register" element={<RegisterIndividual />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={<Login setNoHeaderFooter={setNoHeaderFooter} />}
+        />
         <Route path="/trade-for-me" element={<TradeForMe />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/sell-bitcoin" element={<BuyBitCoin />} />
         <Route path="/buy-bitcoin" element={<SellBitCoin />} />
         <Route path="/transaction" element={<Transaction />} />
-        <Route path="/side-login" element={<SideLogin />} />
-        <Route path="/side-register" element={<SideRegister />} />
+        {/* <Route path="/side-login" element={<SideLogin />} />
+        <Route path="/side-register" element={<SideRegister />} /> */}
         <Route path="/forex-training" element={<ForexTraning />} />
       </Routes>
-      <Footer />
+      {noHeaderFooter ? null : <Footer />}
     </>
-  )
+  );
 }
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,68 +1,87 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import waveBG from "../assets/waveBg.svg";
 import InputBox from "../components/InputBox";
+import loginSvg from "../assets/loginSvg.svg";
 
-export default function Login() {
+export default function Login({ setNoHeaderFooter }) {
+  useEffect(() => {
+    setNoHeaderFooter(true);
+    return () => {
+      setNoHeaderFooter(false);
+    };
+  }, []);
+
   return (
     <>
-      <div className="register__section">
-        <img src={waveBG} alt="waveBG" className="register__section__img" />
-        <div className="register__section__content">
-          {/* <div
-            style={{ color: "#000000" }}
-            className="home__section__carousel__entry__overlay__content__sub__heading"
-          >
-            WELCOME TO
-          </div> */}
+      <div action="" className="login__container">
+        <div className="login__container__left">
+          <div className="login__container__left__heading">
+            Log In With Escroblockh
+          </div>
+
+          <form className="register__section__forms__content__inputs__one">
+            <InputBox
+              placeholder="Your Phone or Email"
+              required={true}
+              type="text"
+            />
+          </form>
+          <div className="register__section__forms__content__inputs__one">
+            <InputBox placeholder="Password" required={true} type="password" />
+          </div>
+          <div className="login__links__wrapper register__section__forms__content__inputs__one">
+            <div>
+              <input
+                className="styled-checkbox"
+                id="styled-checkbox"
+                type="checkbox"
+                name="female"
+              />
+              <label
+                style={{ color: "#1c0a15", fontSize: 16 }}
+                htmlFor="styled-checkbox"
+              >
+                Keep me signed in
+              </label>
+            </div>
+            <Link to="/forgot-password" className="login__forgot">
+              Forgot Password ?
+            </Link>
+          </div>
           <div
-            style={{ color: "#000000" }}
-            className="home__section__carousel__entry__overlay__content__heading"
+            style={{ marginTop: 50 }}
+            className="register__section__forms__content__btns"
           >
-            Escrowblock
+            <Link
+              style={{ padding: "1em 8em" }}
+              to="/transaction"
+              onClick={() => {
+                window.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+                });
+              }}
+              className="button__secondary"
+            >
+              Login
+            </Link>
+            <div style={{ color: "#000000" }} className="new__to__login">
+              New on Escrowblockh?
+              <span>
+                <Link to="/register"> Click here to Register</Link>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <form action="" className="login__container">
-        <div className="register__section__forms__content__inputs__one">
-          <InputBox placeholder="Enter Username" required={true} type="text" />
-        </div>
-        <div className="register__section__forms__content__inputs__one">
-          <InputBox placeholder="Password" required={true} type="password" />
-        </div>
-        <div className="register__section__forms__content__inputs__one">
-          <input
-            className="styled-checkbox"
-            id="styled-checkbox"
-            type="checkbox"
-            name="female"
+        <div className="login__container__right">
+          <img
+            src={loginSvg}
+            alt="loginSvg"
+            className="login__container__right__img"
           />
-          <label
-            style={{ color: "#1c0a15", fontSize: 16 }}
-            htmlFor="styled-checkbox"
-          >
-            Keep me signed in
-          </label>
         </div>
-        <div className="register__section__forms__content__btns">
-          <Link
-            style={{ marginRight: "1em" }}
-            to="/transaction"
-            onClick={() => {
-              window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-            }}
-            className="button__secondary"
-          >
-            Login
-          </Link>
-          <Link to="/register" className="button__secondary">
-            Register
-          </Link>
-        </div>
-      </form>
+      </div>
     </>
   );
 }
